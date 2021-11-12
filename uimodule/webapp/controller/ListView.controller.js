e7869911e9b0978173fc4ea38468058df77e4f55
NixoG Nexed <nixognexed@outlook.de>
2021-11-12 23:51:45 +0100

    implemented listview, routing and api call

---
+++ uimodule/webapp/controller/ListView.controller.js
@@ -0,0 +1,40 @@
+sap.ui.define([
+    "com/myorg/ui5learning/controller/BaseController",
+    'sap/ui/model/json/JSONModel',
+    "sap/ui/core/routing/History"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} BaseController
+     * @param {typeof sap.ui.model.json.JSONModel} JSONModel
+     * @param {typeof sap.ui.core.routing.History} History
+     */
+    function (Controller, JSONModel, History) {
+        "use strict";
+
+        return Controller.extend("com.myorg.ui5learning.controller.ListView", {
+
+            onInit() {
+                var oModel = new JSONModel();
+                this.getView().setModel(oModel, 'characters');
+
+                oModel.loadData('https://api.sampleapis.com/avatar/characters');
+                oModel.attachRequestCompleted(function() {
+                    var data = oModel.getData();
+                    data = data.filter(x => x.hasOwnProperty('name'));
+                    this.getView().getModel('characters').setData(data);
+                }.bind(this))
+            },
+
+            onNavBack() {
+                var oHistory = History.getInstance();
+                var sPreviousHash = oHistory.getPreviousHash();
+
+                if (sPreviousHash !== undefined) {
+                    window.history.back();
+                } else {
+                    this.navTo("RouteMainView", {}, true);
+                }
+            }
+
+        });
+    });
