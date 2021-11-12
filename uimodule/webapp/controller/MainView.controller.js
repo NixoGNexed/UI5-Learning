f68768644499633eade8b871464b472681d49ef5
NixoG Nexed <nixognexed@outlook.de>
2021-11-12 14:46:15 +0100

    Added Content to MainView

--- uimodule/webapp/controller/MainView.controller.js
+++ uimodule/webapp/controller/MainView.controller.js
@@ -1,14 +1,37 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    'sap/ui/model/json/JSONModel'
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
+     * @param {typeof sap.ui.model.json.JSONModel} JSONModel
      */
-    function (Controller) {
+    function (Controller, JSONModel) {
         "use strict";
 
         return Controller.extend("com.myorg.ui5learning.controller.MainView", {
+
             onInit: function () {
+                var oData = {
+                    "data": [
+                        {
+                            "Name": "Create UI5-App"
+                        },
+                        {
+                            "Name": "Work with O-Data"
+                        },
+                        {
+                            "Name": "Try out Web-Components"
+                        }
+                    ]
+                }
+
+
+                var oModel = new JSONModel(oData);
+                this.getView().setModel(oModel, "data");
+            },
+
+            onPressNavigateToList: function () {
 
             }
         });
